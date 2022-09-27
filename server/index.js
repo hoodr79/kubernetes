0a782808280c2db7c1b135dc431a922174e809ae
hoodr792 <hoodr7987@gmail.com>
2022-09-27 10:39:00 +0530

    make it better

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require("express");
+
+process.env.WP_DISABLE_POLLING=1;
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// Have Node serve the files for our built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (_req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+  
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+
+});
+
